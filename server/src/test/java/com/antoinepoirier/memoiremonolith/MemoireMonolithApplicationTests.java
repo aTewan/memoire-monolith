fc26075676bd722f63caf8bc0c8adc0eea8d588d
aTewan <tewan33@gmail.com>
2020-05-05 19:15:52 +0200

    :truck: Création d'un dossier server

---
+++ server/src/test/java/com/antoinepoirier/memoiremonolith/MemoireMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antoinepoirier.memoiremonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoireMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
